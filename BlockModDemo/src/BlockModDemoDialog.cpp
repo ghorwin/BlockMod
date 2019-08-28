519f5d02b3e96b9b8c154a85e56152e73e5a3b32
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2019-08-28 10:54:30 +0200

    Added button icon resources for demo.

--- BlockModDemo/src/BlockModDemoDialog.cpp
+++ BlockModDemo/src/BlockModDemoDialog.cpp
@@ -1,11 +1,15 @@
 #include "BlockModDemoDialog.h"
 #include "ui_BlockModDemoDialog.h"
 
+#include <QGridLayout>
+
 BlockModDemoDialog::BlockModDemoDialog(QWidget *parent) :
-	QDialog(parent),
+	QDialog(parent, Qt::Window | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint),
 	ui(new Ui::BlockModDemoDialog)
 {
 	ui->setupUi(this);
+	ui->gridLayout_2->setColumnStretch(0,1);
+	ui->gridLayout_2->setRowStretch(1,1);
 }
 
 BlockModDemoDialog::~BlockModDemoDialog()
